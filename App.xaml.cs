cdc8844f4e70f905461fdda8c61c2ce7ab09a08f
Abhishek K <abhi30702@gmail.com>
2026-07-27 22:47:10 +0530

    feat: .NET 8 WPF scaffold with tray icon (M0)

---
+++ App.xaml.cs
@@ -0,0 +1,36 @@
+using System.Windows;
+using System.Windows.Forms;
+
+namespace WhisperFlowLocal;
+
+public partial class App : System.Windows.Application
+{
+    private NotifyIcon? _trayIcon;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        System.Drawing.Icon? icon = null;
+        try
+        {
+            var stream = GetResourceStream(new Uri("Resources/tray-icon.ico", UriKind.Relative))?.Stream;
+            if (stream != null) icon = new System.Drawing.Icon(stream);
+        }
+        catch { }
+
+        _trayIcon = new NotifyIcon
+        {
+            Icon = icon ?? SystemIcons.Application,
+            Visible = true,
+            Text = "Whisper Flow Local"
+        };
+        _trayIcon.DoubleClick += (_, _) => Shutdown();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _trayIcon?.Dispose();
+        base.OnExit(e);
+    }
+}
