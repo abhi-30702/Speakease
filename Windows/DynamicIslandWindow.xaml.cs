8ca84e43df225655880e9e8aed150e947d6837b5
Abhishek K <abhi30702@gmail.com>
2026-08-02 12:05:25 +0530

    feat(m3a): add DynamicIslandWindow (replaces PillWindow)

---
+++ Windows/DynamicIslandWindow.xaml.cs
@@ -0,0 +1,134 @@
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Microsoft.Win32;
+using WhisperFlowLocal.Models;
+using WhisperFlowLocal.ViewModels;
+
+namespace WhisperFlowLocal.Windows;
+
+public partial class DynamicIslandWindow : Window
+{
+    private readonly DynamicIslandViewModel _vm;
+    private readonly DispatcherTimer _waveformTimer;
+    private readonly Storyboard _spinnerAnim;
+    private Rectangle[] _bars = [];
+
+    public DynamicIslandWindow(DynamicIslandViewModel vm)
+    {
+        InitializeComponent();
+        _vm = vm;
+
+        _waveformTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(80) };
+        _waveformTimer.Tick += OnWaveformTick;
+
+        // Clone so the resource Storyboard is independently controllable
+        _spinnerAnim = ((Storyboard)FindResource("SpinnerAnim")).Clone();
+
+        Loaded += (_, _) => _bars = [Bar0, Bar1, Bar2, Bar3, Bar4, Bar5, Bar6, Bar7, Bar8, Bar9];
+        vm.PropertyChanged += OnVmPropertyChanged;
+        SystemEvents.DisplaySettingsChanged += (_, _) => Dispatcher.Invoke(PositionBottomCenter);
+
+        PositionBottomCenter();
+    }
+
+    private void OnVmPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(DynamicIslandViewModel.State))
+            Dispatcher.Invoke(UpdateVisualState);
+        if (e.PropertyName == nameof(DynamicIslandViewModel.ErrorMessage) &&
+            _vm.State == RecordingState.Error)
+            Dispatcher.Invoke(() => ErrorContent.Text = _vm.ErrorMessage);
+    }
+
+    private void UpdateVisualState()
+    {
+        switch (_vm.State)
+        {
+            case RecordingState.Idle:
+                _waveformTimer.Stop();
+                _spinnerAnim.Stop(this);
+                HideCapsule();
+                break;
+
+            case RecordingState.Recording:
+                RecordingContent.Visibility    = Visibility.Visible;
+                TranscribingContent.Visibility = Visibility.Collapsed;
+                ErrorContent.Visibility        = Visibility.Collapsed;
+                ShowCapsule(200);
+                _waveformTimer.Start();
+                _spinnerAnim.Stop(this);
+                break;
+
+            case RecordingState.Transcribing:
+            case RecordingState.Inserting:
+                RecordingContent.Visibility    = Visibility.Collapsed;
+                TranscribingContent.Visibility = Visibility.Visible;
+                ErrorContent.Visibility        = Visibility.Collapsed;
+                ShowCapsule(160);
+                _waveformTimer.Stop();
+                _spinnerAnim.Begin(this, true);
+                break;
+
+            case RecordingState.Error:
+                ErrorContent.Text              = _vm.ErrorMessage;
+                RecordingContent.Visibility    = Visibility.Collapsed;
+                TranscribingContent.Visibility = Visibility.Collapsed;
+                ErrorContent.Visibility        = Visibility.Visible;
+                ShowCapsule(220);
+                _waveformTimer.Stop();
+                _spinnerAnim.Stop(this);
+                ScheduleErrorDismiss();
+                break;
+        }
+    }
+
+    private void ShowCapsule(double targetWidth)
+    {
+        double fromWidth = Capsule.Visibility == Visibility.Collapsed ? 0 : Capsule.ActualWidth;
+        Capsule.Visibility = Visibility.Visible;
+        var anim = new DoubleAnimation(fromWidth, targetWidth, TimeSpan.FromMilliseconds(220))
+        {
+            EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+        };
+        Capsule.BeginAnimation(WidthProperty, anim);
+    }
+
+    private void HideCapsule()
+    {
+        if (Capsule.Visibility == Visibility.Collapsed) return;
+        var anim = new DoubleAnimation(Capsule.ActualWidth, 0, TimeSpan.FromMilliseconds(180))
+        {
+            EasingFunction = new CubicEase { EasingMode = EasingMode.EaseIn }
+        };
+        anim.Completed += (_, _) => Capsule.Visibility = Visibility.Collapsed;
+        Capsule.BeginAnimation(WidthProperty, anim);
+    }
+
+    private void ScheduleErrorDismiss()
+    {
+        var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(3) };
+        timer.Tick += (_, _) =>
+        {
+            timer.Stop();
+            if (_vm.State == RecordingState.Error)
+                HideCapsule();
+        };
+        timer.Start();
+    }
+
+    private void OnWaveformTick(object? sender, EventArgs e)
+    {
+        foreach (var bar in _bars)
+            bar.Height = Random.Shared.Next(4, 17);
+    }
+
+    private void PositionBottomCenter()
+    {
+        var wa = SystemParameters.WorkArea;
+        Left = wa.Left + (wa.Width - Width) / 2;
+        Top  = wa.Bottom - Height - 24;
+    }
+}
